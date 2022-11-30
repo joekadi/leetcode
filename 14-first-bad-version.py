846de4f308d57cb520966e4370353804124ec360
Joe Kadi <joe.kadi1@nhs.net>
2022-11-30 17:29:38 +0000

    is bad version

---
+++ 14-first-bad-version.py
@@ -0,0 +1,23 @@
+#https://leetcode.com/problems/first-bad-version/
+#The isBadVersion API is already defined for you.
+#def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        l, r = 1, n
+        
+        
+        while l <= r:
+            mid = round((l+r)/2)
+            
+            bad_version = isBadVersion(mid)
+            
+            if bad_version:
+                r = mid - 1
+            else:
+                l = mid +1
+                
+        return l
+    
+    
+            
